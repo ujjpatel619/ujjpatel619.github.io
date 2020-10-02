2f504554b91391f310661f351a367e05274f4263
Coding <Coding@T-C02C9CB0MD6R.local>
2020-10-03 00:13:55 +0100

    My Second Commit

---
+++ practice.js
@@ -0,0 +1,27 @@
+/* 
+Variables: Number, String, Boolean, Undefined, Null
+*/
+
+let name = "Ujjaval";
+const age = 22;
+let isMarried = false;
+let email = null;
+let avatar = undefined;
+
+
+/* 
+Objects, it has key:pair values.
+*/ 
+
+const person = {
+    name: 'Ujjaval',
+    age: 22,
+    avatar: undefined
+}
+
+console.log('Hello ' + person.name);
+
+
+/* 
+Objects, it has key:pair values.
+*/
