2a60353e62846c504a1acec6427252e4bfaddc34
Ujjaval <ujjavalpatel@hotmail.com>
2021-01-17 17:13:45 +0000

    Adding data and calc js file

--- practice.js
+++ practice.js
@@ -1,33 +1,43 @@
-/* 
-Variables: Number, String, Boolean, Undefined, Null
-*/
+//Variables 
 
-let name = "Ujjaval";
-const age = 22;
-let isMarried = false;
-let email = null;
-let avatar = undefined;
+// Primitive Type
+let name = 'Ujjaval'; //String
+let age = 20; //Number
+let isOnline = true; //Boolean
+let address = undefined; //Undefined
+let water = null; // Null
 
-
-/* 
-Objects, it has key:pair values.
-*/ 
-
-const person = {
-    name: 'Ujjaval',
-    age: 22,
-    avatar: undefined
+// Reference Type
+//Object 
+const indianGod = {
+ avatar1: {
+     name: 'Dashama'
+ },
+ avatar2: {
+     name: 'Kalima'
+ },
+ avatar3: {
+    name: 'Laxmima'
+},
+avatar4: {
+    name: 'Digambaraya'
+},
+religion: 'Hindu',
+Author: 'Ujjaval',
+age: function totalAge(a) {
+    return age + a
 }
-
-console.log('Hello ' + person.name);
-
-
-/* 
-Functions.
-*/
-
-function world() {
-    console.log('Hello World')
 }
+//console.log(indianGod.avatar4.name)
+console.log(indianGod.age(5))
 
-world();
+//Array
+let favFruits = ['apple', 'banana', 'grapes', 'oranges', 4]
+favFruits.push('newEntry.')
+// console.log(favFruits)
+
+//Function 
+function demo1() {
+    console.log('This is a function demo!')
+}
+demo1();
