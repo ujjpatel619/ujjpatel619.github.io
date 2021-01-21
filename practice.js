e36a43f1e3974b96843d1fb683d179b47bb16aa7
ujjpatel <ujjavalpatel@hotmail.com>
2021-01-21 17:48:52 +0000

    Adding new fuctions

--- practice.js
+++ practice.js
@@ -46,23 +46,49 @@
 
 //Result Calculator
 
-function examResult(input) {
-if(input < 40) {
-    console.log('You have failed.')
-} else if (input >= 40 && input < 50) {
-console.log('just passed! ')
-} else if (input >= 50 && input < 60) {
-    console.log('got a 2:2! ')
-} else if (input >= 60 && input < 70) {
-    console.log('got a 2:1! ')
-} else if (input >= 70 && input <= 100) {
-        console.log('got a 1:1! ')
-} else {console.log('Enter correct digits!')}}
+// function examResult(input) {
+// if(input < 40) {
+//     console.log('You have failed.')
+// } else if (input >= 40 && input < 50) {
+// console.log('just passed! ')
+// } else if (input >= 50 && input < 60) {
+//     console.log('got a 2:2! ')
+// } else if (input >= 60 && input < 70) {
+//     console.log('got a 2:1! ')
+// } else if (input >= 70 && input <= 100) {
+//         console.log('got a 1:1! ')
+// } else {console.log('Enter correct digits!')}}
 
-examResult(34)
+// examResult(34)
 
-function demoDom() {
-    let al = alert('Hello there' )
-document.getElementById('tst1').innerHTML = al }
+// function demoDom() {
+//     let al = alert('Hello there' )
+// document.getElementById('tst1').innerHTML = al }
 
-console.log('this is work macbook!');
+// console.log('this is work macbook!');
+
+
+function pingPong() {
+for(number = 0; number < 100; number++){
+    if(number % 2) {
+        console.log(number + ' IS ODD!')
+    }
+    else{
+        console.log(number + ' IS EVEN!')
+    }
+    console.log('')
+}
+}
+pingPong()
+
+function oddOrEven() {
+    let numberInput = document.getElementById('numInput').value;
+    let numOutput = document.getElementById('numOutput')
+    if(numberInput % 2){
+        numOutput.innerText = numberInput + ' IS ODD NUMBER'
+        // console.log(numberInput + ' IS ODD NUMBER')
+    }else{
+        numOutput.innerText = numberInput + ' IS EVEN NUMBER'
+        // console.log(numberInput + ' IS EVEN NUMBER')
+    }
+}
